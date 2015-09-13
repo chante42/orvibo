71b4df5f70d88e881b1fc4133940191cae11e4a9
Olivier Chanteloup <chante42@gmail.com>
2015-09-13 18:59:52 +0200

    new file:   go.cmd
    	Fichier de lancement automatique pour le serveur S.A.R.A.H.

---
+++ go.cmd
@@ -0,0 +1,14 @@
+SET NODE_PATH=C:\S.A.R.A.H.V3.1.6\NodeJS\node_modules\npm\node_modules;C:\S.A.R.A.H.V3.1.6\NodeJS\node_modules\npm
+
+
+SET NODE_PATH=%NODE_PATH%;%cd%\ExpressJS\node_modules
+
+
+SET NODE_PATH=%NODE_PATH%;%cd%\script\lib;%cd%\script\vendor
+
+
+cd  "C:\orvibo"
+
+
+
+"C:\S.A.R.A.H.V3.1.6\NodeJS\node.exe" server.js
